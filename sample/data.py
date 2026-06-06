6ee3836bc104d7c4774c89c7babce7f39d9c1d12
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-06-06 23:58:51 +0530

    Create data.py

---
+++ sample/data.py
@@ -0,0 +1,4 @@
+transaction_id,customer_id,amount,country,device_type,event_time,ingest_time
+1,102,220.50,India,ANDROID,2026-05-01 10:00:00,2026-05-01 10:00:20
+2,450,1500.75,USA,WEB,2026-05-01 11:00:00,2026-05-01 11:00:10
+3,920,720.00,Germany,IPHONE,2026-05-01 12:00:00,2026-05-01 12:00:35
